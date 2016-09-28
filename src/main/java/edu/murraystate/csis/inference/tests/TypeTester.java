6fe8d9c9b8da04dcb1b74eb800a62cd7a75de670
Phillip Wright <pwright@murraystate.edu>
2016-09-28 09:53:42 -0500

    Remove redundant code in TypeTester
    Since we are handling aggregation and streaming with adapters, the List and Stream methods in TypeTester are redundant, so I have removed them and their corresponding tests.

--- src/main/java/edu/murraystate/csis/inference/tests/TypeTester.java
+++ src/main/java/edu/murraystate/csis/inference/tests/TypeTester.java
@@ -1,21 +1,7 @@
 package edu.murraystate.csis.inference.tests;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public interface TypeTester {
     TestResult test(final String sample);
-
-    default List<TestResult> test(final List<String> samples) {
-        return samples.stream()
-                .map(this::test)
-                .collect(Collectors.toList());
-     }
-
-    default Stream<TestResult> test(final Stream<TestResult> input) {
-        return input.map(this::test);
-    }
 
     default TestResult test(final TestResult input) {
         return input.mergeWith(test(input.getSample()));
--- src/test/java/edu/murraystate/csis/inference/tests/TypeTesterTest.java
+++ src/test/java/edu/murraystate/csis/inference/tests/TypeTesterTest.java
@@ -3,30 +3,10 @@ package edu.murraystate.csis.inference.tests;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class TypeTesterTest {
-    @Test
-    public void aggregateTest(){
-        final TypeTester t = new DummyTest();
-        final List<String> samples = new ArrayList<>();
-        samples.add("input1");
-        samples.add("input2");
-        samples.add("input3");
-
-        final List<TestResult> results = t.test(samples);
-        Assert.assertEquals(3, results.size());
-        for (final TestResult tr : results) {
-            Assert.assertEquals(1, tr.getPossibleTypes().size());
-            Assert.assertTrue(tr.getPossibleTypes().contains("test"));
-        }
-    }
-
     @Test
     public void testResultAsInput() {
         final String inputSample = "testSample";
@@ -37,24 +17,6 @@ public class TypeTesterTest {
         Assert.assertEquals(1, result.getPossibleTypes().size());
         Assert.assertTrue(result.getPossibleTypes().contains("test"));
         Assert.assertEquals(inputSample, result.getSample());
-    }
-
-    @Test
-    public void testWithStream() {
-        final TypeTester t = new DummyTest();
-        final List<String> samples = new ArrayList<>();
-        samples.add("input1");
-        samples.add("input2");
-        samples.add("input3");
-
-        final Stream<TestResult> inputStream = samples.stream().map(TestResult::new);
-        final List<TestResult> results = t.test(inputStream).collect(Collectors.toList());
-
-        Assert.assertEquals(3, results.size());
-        for (final TestResult tr : results) {
-            Assert.assertEquals(1, tr.getPossibleTypes().size());
-            Assert.assertTrue(tr.getPossibleTypes().contains("test"));
-        }
     }
 
     public class DummyTest implements TypeTester{
