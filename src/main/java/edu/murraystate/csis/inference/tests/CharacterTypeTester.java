adeaef9b9c0bd921b7008a273891602a7ad7b391
brickard15 <brian.rick@gmail.com>
2016-11-06 00:52:49 -0500

    I used a lambda expression to parse the a character in the constructor.

--- src/main/java/edu/murraystate/csis/inference/tests/CharacterTypeTester.java
+++ src/main/java/edu/murraystate/csis/inference/tests/CharacterTypeTester.java
@@ -1,8 +1,21 @@
 package edu.murraystate.csis.inference.tests;
 
-public class CharacterTypeTester extends TryCatchTester<Character> {
+public final class CharacterTypeTester extends TryCatchTester<Character> {
 
     public CharacterTypeTester() {
-        super("Character", Character::parseCharacter);
+        super(
+            "Character", 
+            (String input) -> {
+                Character characterResult = ' ';
+                final int characterLength = 1;
+
+                if(input.length() == characterLength){
+                    Character firstCharacter = input.charAt(0);
+                    characterResult = firstCharacter;
+                }
+
+                return characterResult;    
+            }
+            );
     }
 }
