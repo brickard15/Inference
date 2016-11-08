636f39ed46192fb978886de4ff939f839400d05d
brickard15 <brian.rick@gmail.com>
2016-11-07 23:19:32 -0600

    Created the tests for the BooleanTypeTester. I implemented two tests for the Valid input of true and false, and a test for the invalid case.

---
+++ src/test/java/edu/murraystate/csis/inference/tests/BooleanTypeTesterTest.java
@@ -0,0 +1,47 @@
+package edu.murraystate.csis.inference.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BooleanTypeTesterTest {
+    @Test
+    public void validTrueTest() {
+        final String input = "true";
+        final TypeTester booleanTester = new BooleanTypeTester();
+        final TestResult result = booleanTester.test(input);
+        
+        Assert.assertFalse(
+            result.getPossibleTypes().isEmpty()
+        );
+        
+        Assert.assertTrue(
+            result.getPossibleTypes().contains("Boolean")
+        );
+    }
+    
+    @Test
+    public void validFalseTest() {
+        final String input = "false";
+        final TypeTester booleanTester = new BooleanTypeTester();
+        final TestResult result = booleanTester.test(input);
+        
+        Assert.assertFalse(
+            result.getPossibleTypes().isEmpty()
+        );
+        
+        Assert.assertTrue(
+            result.getPossibleTypes().contains("Boolean")
+        );
+    }
+    
+    @Test
+    public void invalidTest() {
+        final String input = "False";
+        final TypeTester booleanTester = new BooleanTypeTester();
+        final TestResult result = booleanTester.test(input);
+        
+        Assert.assertTrue(
+                result.getPossibleTypes().isEmpty()
+        );
+    }
+}
