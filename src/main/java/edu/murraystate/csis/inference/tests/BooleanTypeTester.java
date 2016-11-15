978d9461ac3df3e7cd55766aec8134d31235b2a8
brickard15 <brian.rick@gmail.com>
2016-11-14 21:57:01 -0600

    Added Exception Handling for the BooleanTypeTester
    I created a helper method parseBoolean to use in the test method. In the test method I call the parseBoolean method and check for exceptions.

--- src/main/java/edu/murraystate/csis/inference/tests/BooleanTypeTester.java
+++ src/main/java/edu/murraystate/csis/inference/tests/BooleanTypeTester.java
@@ -5,10 +5,17 @@ import java.util.Set;
 
 public class BooleanTypeTester implements TypeTester{
     private static final String typeString = "Boolean";
-
+    
     @Override
-    public TestResult test(final String sample) {
-        
+    public TestResult test(final String sample) { 
+        try{
+            return parseBoolean(sample);
+        }catch(Exception e){
+            return new TestResult(sample);
+        }
+    }
+    
+    private TestResult parseBoolean(final String sample){
         if(sample.contentEquals("true") || sample.contentEquals("false")){
             Set<String> possibleTypes = new HashSet<>();
             possibleTypes.add(typeString);
