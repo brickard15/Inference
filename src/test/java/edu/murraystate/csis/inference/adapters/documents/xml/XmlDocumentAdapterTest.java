7819f7d6b424eae1056cd0ed13b773f4cf32ad3d
brickard <brickard@BB454-110970.msunet2k.edu>
2017-02-06 10:45:34 -0600

    Created a simpleTest for the XMLAdapterTest class.

--- src/test/java/edu/murraystate/csis/inference/adapters/documents/xml/XmlDocumentAdapterTest.java
+++ src/test/java/edu/murraystate/csis/inference/adapters/documents/xml/XmlDocumentAdapterTest.java
@@ -1,16 +1,34 @@
 package edu.murraystate.csis.inference.adapters.documents.xml;
 
-import edu.murraystate.csis.inference.adapters.documents.DocumentAdapter;
-import edu.murraystate.csis.inference.tests.TypeTester;
-import java.util.stream.Stream;
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.junit.Assert.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import static org.mockito.Mockito.mock;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
 
 public class XmlDocumentAdapterTest {
+    
     @Test
-    public void simpleTest(){
+    public void simpleTest() throws IOException, SAXException, ParserConfigurationException{
+        File tempXMLFile = File.createTempFile("Prefix", "Suffix");
+        PrintWriter XMLWriter = new PrintWriter(tempXMLFile);
+        XMLWriter.println("<name>John</name>");
+        XMLWriter.close();
+                
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(tempXMLFile);
+        
+        Node documentNode = document.getFirstChild();
+        String textContent = documentNode.getTextContent();
+        
+        assertEquals("John", textContent);
         
     }
 }
