25188a3f802775a8eefb39994f73fc92dc18adde
brickard <brickard@BB454-110971.msunet2k.edu>
2017-02-13 10:37:38 -0600

    Created a proof of concept Test using the XMLEventreader class.

--- src/test/java/edu/murraystate/csis/inference/adapters/documents/xml/XmlDocumentAdapterTest.java
+++ src/test/java/edu/murraystate/csis/inference/adapters/documents/xml/XmlDocumentAdapterTest.java
@@ -1,34 +1,41 @@
 package edu.murraystate.csis.inference.adapters.documents.xml;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import java.io.Reader;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.XMLEvent;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.xml.sax.SAXException;
+
 
 public class XmlDocumentAdapterTest {
-    
+
     @Test
-    public void simpleTest() throws IOException, SAXException, ParserConfigurationException{
+    public void simpleXMLEventReaderTest() throws FileNotFoundException, IOException, XMLStreamException{
         File tempXMLFile = File.createTempFile("Prefix", "Suffix");
         PrintWriter XMLWriter = new PrintWriter(tempXMLFile);
         XMLWriter.println("<name>John</name>");
-        XMLWriter.close();
-                
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-        Document document = documentBuilder.parse(tempXMLFile);
+        XMLWriter.close();  
+        Reader xmlFileReader = new FileReader(tempXMLFile);
+        String resultText = "false";
         
-        Node documentNode = document.getFirstChild();
-        String textContent = documentNode.getTextContent();
+        XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+        XMLEventReader eventReader = xmlInputFactory.createXMLEventReader(xmlFileReader);
         
-        assertEquals("John", textContent);
+        while(eventReader.hasNext()){
+            XMLEvent nextEvent = eventReader.nextEvent();
+            if(nextEvent.isCharacters()){
+                resultText = nextEvent.toString();
+            }
+        }
         
+        assertEquals("John", resultText);
     }
 }
+
