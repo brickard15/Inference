4d0f17d7c3dafbc03ad7a0802bdeaafa49dc383d
brickard <brickard@BB454-110971.msunet2k.edu>
2017-02-13 14:34:13 -0600

    Implemented the Iterator Interface, a constructor, and hasNext method.
    I implemented the Iterator Interface. I created a constructor that takes
    a reader. I implemented the hasNext method by adapting the hasNext method
    from the XMLEventReader class.

--- src/main/java/edu/murraystate/csis/inference/adapters/documents/xml/XMLTupleIterator.java
+++ src/main/java/edu/murraystate/csis/inference/adapters/documents/xml/XMLTupleIterator.java
@@ -1,5 +1,35 @@
 package edu.murraystate.csis.inference.adapters.documents.xml;
 
-public class XMLTupleIterator {
+import fj.P2;
+import java.io.Reader;
+import java.util.Iterator;
+import java.util.Optional;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+
+public class XMLTupleIterator implements Iterator<Optional<P2<String, String>>>{
+
+    final private XMLEventReader xmlEventReader;
+    final private Reader reader;
+    
+    public XMLTupleIterator(Reader reader) throws XMLStreamException{
+        try {
+            this.reader = reader;
+            xmlEventReader = XMLInputFactory.newFactory().createXMLEventReader(reader);
+        } catch (XMLStreamException ex) {
+            throw new XMLStreamException("Invalid Reader");
+        }
+    }
+    
+    @Override
+    public boolean hasNext() {
+        return xmlEventReader.hasNext();
+    }
+
+    @Override
+    public Optional<P2<String, String>> next() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
