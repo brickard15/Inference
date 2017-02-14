8235789f924972a623c84050981367914718db0c
Brian Rickard <brian.rick@gmail.com>
2017-02-13 22:20:47 -0600

    I created the XMLTupleIteratorTest class, a setupXMLReaderWithOneTag method, and a testGetValueWithOneTag method.
    The setupXMLReaderWithOneTag method is used in the testGetValueWithOneTag method.

---
+++ src/test/java/edu/murraystate/csis/inference/adapters/documents/xml/XMLTupleIteratorTest.java
@@ -0,0 +1,34 @@
+package edu.murraystate.csis.inference.adapters.documents.xml;
+
+import org.junit.Test;
+import javax.xml.stream.XMLStreamException;
+import java.io.*;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class XMLTupleIteratorTest {
+
+    public FileReader setupXMLReaderWithOneTag() throws IOException {
+        File tempFile = File.createTempFile("XML", "TempFile");
+        PrintWriter tempWriter = new PrintWriter(tempFile);
+        tempWriter.println("<name>John</name>");
+        tempWriter.close();
+
+        return new FileReader(tempFile);
+    }
+
+    @Test
+    public void testGetValueWithOneTag() throws IOException, XMLStreamException {
+        FileReader fileReader = setupXMLReaderWithOneTag();
+        XMLTupleIterator xmlTupleIterator = new XMLTupleIterator(fileReader);
+
+        Optional<String> possibleResult = xmlTupleIterator.getValue();
+        assertTrue(possibleResult.isPresent());
+
+        String result = possibleResult.get();
+        assertEquals("John", result);
+
+    }
+}
