84247a8cc9623d55e2e180b06ef687db4f08b7b8
brickard15 <brian.rick@gmail.com>
2017-02-20 10:21:16 -0600

    Created a testGetValueWithComplexContent method.

--- src/test/java/edu/murraystate/csis/inference/adapters/documents/xml/XMLTupleIteratorTest.java
+++ src/test/java/edu/murraystate/csis/inference/adapters/documents/xml/XMLTupleIteratorTest.java
@@ -3,6 +3,9 @@ package edu.murraystate.csis.inference.adapters.documents.xml;
 import org.junit.Test;
 import javax.xml.stream.XMLStreamException;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
@@ -73,5 +76,46 @@ public class XMLTupleIteratorTest {
 
         Optional<String> possibleResult = xmlTupleIterator.getValue();
         assertFalse(possibleResult.isPresent());
+    }
+
+    @Test
+    public void testGetValueWithComplexContent() throws IOException, XMLStreamException {
+        Reader xmlReader = setupXMLReaderWithComplexContent();
+        XMLTupleIterator xmlTupleIterator = new XMLTupleIterator(xmlReader);
+
+        List<String> results = new ArrayList<>();
+        results.add("John");
+        results.add("Doe");
+        results.add("Murray");
+        results.add("KY");;
+
+        Optional<String> possibleResult = xmlTupleIterator.getValue();
+        assertTrue(possibleResult.isPresent());
+        String result1 = possibleResult.get();
+        assertEquals(results.get(0), result1);
+
+        Optional<String> possibleResult2 = xmlTupleIterator.getValue();
+        assertTrue(possibleResult2.isPresent());
+        String result2 = possibleResult2.get();
+        assertEquals(results.get(1), result2);
+
+        Optional<String> possibleResult3 = xmlTupleIterator.getValue();
+        assertTrue(possibleResult3.isPresent());
+        String result3 = possibleResult3.get();
+        assertEquals(results.get(2), result3);
+
+        Optional<String> possibleResult4 = xmlTupleIterator.getValue();
+        assertTrue(possibleResult4.isPresent());
+        String result4 = possibleResult4.get();
+        assertEquals(results.get(3), result4);
+    }
+
+    public Reader setupXMLReaderWithComplexContent() throws IOException {
+        File tempFile = File.createTempFile("XML", "ReaderWithNestedTags");
+        PrintWriter tempWriter = new PrintWriter(tempFile);
+        tempWriter.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?><bio><name><first>John</first><last>Doe</last></name></bio><address><city>Murray</city><state>KY</state></address>");
+        tempWriter.close();
+
+        return new FileReader(tempFile);
     }
 }
